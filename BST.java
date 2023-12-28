d14c2f3a61dd1f3a25b9f6e7ecc6b6d08b3bdfa4
prarthanagalani <prarthanagalani@gmail.com>
2023-12-28 11:56:01 +0530

    added: search function-day10

--- BST.java
+++ BST.java
@@ -50,6 +50,22 @@ class BinarySearchTree<T extends Comparable<T>> {
 
         return 1 + sizeRec(root.left) + sizeRec(root.right);
     }
+
+    // method to search node
+    boolean search(T value) {
+        return searchRec(root, value);
+    }
+
+    // recursive method to search node
+    boolean searchRec(MyBinaryNode<T> root, T value) {
+        if (root == null)
+            return false;
+
+        if (root.key.equals(value))
+            return true;
+
+        return searchRec(root.left, value) || searchRec(root.right, value);
+    }
 }
 
 public class BST {
@@ -68,7 +84,17 @@ public class BST {
         bst.insert(60);
         bst.insert(95);
 
+        // uc2
         System.out.println("Size: " + bst.size());
+
+        // search for node 60-uc3
+        boolean isFound = bst.search(60);
+
+        if (isFound) {
+            System.out.println("value 60 is present in BST");
+        } else {
+            System.out.println("value 60 is not present in BST");
+        }
 
     }
 }
